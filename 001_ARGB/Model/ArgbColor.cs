8163688bafd8983c30b96dd8c98eded1e0e0ad54
Yurii Krutsko <yuri.krutsko@gmail.com>
2021-11-05 17:48:38 +0300

    Model released

---
+++ 001_ARGB/Model/ArgbColor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace _001_ARGB.Model
+{
+    class ArgbColor
+    {
+        public byte A { get; set; }
+        public byte R { get; set; }
+        public byte G { get; set; }
+        public byte B { get; set; }
+
+        public ArgbColor() { }
+        public ArgbColor(byte a, byte r, byte g, byte b)
+            => (A, R, G, B) = (a, r, g, b);
+
+        public Color GetColor()
+        {
+            Color color = Color.FromArgb(A, R, G, B);
+            return color;
+        }
+
+        public override string ToString()
+        {
+            Color color = GetColor();
+            return color.A.ToString("X2") + color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
+        }
+    }
+}
---
+++ 001_ARGB/Model/ArgbColorRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_ARGB.Model
+{
+    class ArgbColorRepository
+    {
+        private static ObservableCollection<ArgbColor> _argbColors;
+
+        public static ObservableCollection<ArgbColor> AllColors
+        {
+            get
+            {
+                if (_argbColors == null)
+                {
+                    _argbColors = generateClientRepository();
+                }
+                return _argbColors;
+            }
+        }
+
+        private static ObservableCollection<ArgbColor> generateClientRepository()
+        {
+            ObservableCollection<ArgbColor> argbColors = new ObservableCollection<ArgbColor>();
+
+            argbColors.Add(new ArgbColor(200,10,200,12));
+            argbColors.Add(new ArgbColor(100,15,200,255));
+            argbColors.Add(new ArgbColor(200,200,200,200));
+            argbColors.Add(new ArgbColor(255,15,15,250));
+
+            return argbColors;
+        }
+    }
+}
