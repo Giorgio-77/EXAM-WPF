fda5c1985c73d0dc6c64d7451bb1648030b49e75
Yurii Krutsko <yuri.krutsko@gmail.com>
2021-11-05 16:54:45 +0300

    Add project files.

---
+++ 001_ARGB/DoubleToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace _001_ARGB
+{
+    public class DoubleToColorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Color.FromArgb((byte)(double)values[0], (byte)(double)values[1], (byte)(double)values[2], (byte)(double)values[3]);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            Color C = (Color)value;
+            return new object[] { (double)C.ScA, (double)C.ScR, (double)C.ScG, (double)C.ScB };
+        }
+    }
+}
