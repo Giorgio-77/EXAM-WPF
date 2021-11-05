4b83b93d2116c9808d09ef42ea45c9969ce3fd3c
Yurii Krutsko <yuri.krutsko@gmail.com>
2021-11-05 18:19:35 +0300

    ViewModel Released

---
+++ 001_ARGB/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,71 @@
+﻿using _001_ARGB.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_ARGB.ViewModel
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        ObservableCollection<ArgbColor> _argbColor;
+
+        public ObservableCollection<ArgbColor> ArgbColors
+        {
+            get
+            {
+                if (_argbColor == null)
+                    _argbColor = ArgbColorRepository.AllColors;
+                return _argbColor;
+            }
+        }
+
+        ArgbColor _currentColor;
+
+        public ArgbColor CurrentClient
+        {
+            get
+            {
+                if (_currentColor == null)
+                {
+                    _currentColor = new ArgbColor();
+                }
+                return _currentColor;
+            }
+            set
+            {
+                _currentColor = value;
+                OnPropertyChanged(nameof(CurrentClient));
+            }
+        }
+
+        ArgbColor _selectedColor;
+
+        public ArgbColor SelectedClient
+        {
+            get
+            {
+                if (_selectedColor == null)
+                {
+                    _selectedColor = new ArgbColor();
+                }
+                return _selectedColor;
+            }
+            set
+            {
+                _selectedColor = value;
+                OnPropertyChanged(nameof(SelectedClient));
+            }
+        }
+
+
+
+
+        protected override void OnDispose()
+        {
+            this.ArgbColors.Clear();
+        }
+    }
+}
---
+++ 001_ARGB/ViewModel/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_ARGB.ViewModel
+{
+    class ViewModelBase: INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Dispose()
+        {
+            this.OnDispose();
+        }
+
+        protected virtual void OnDispose() { }
+
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
